0f71e147a3c37255d878ac2054985c3c9bac929b
Tetsuro Kurita <tkurita@mac.com>
2006-07-31 11:12:13 +0000

    [DonationReminder] move DonationReminder.m to trunk
    git-svn-id: file:///Users/tkurita/Dev/svnrepos/DonationReminder/trunk@1203 a4934b6f-310c-0410-bed4-ac220e363503

---
+++ DonationReminder.m
@@ -0,0 +1,79 @@
+#import "DonationReminder.h"
+
+@implementation DonationReminder
+
++ (void)goToDonation
+{
+	NSString *urlString = [[NSUserDefaults standardUserDefaults] stringForKey:@"DonationURL"];
+	NSAssert(urlString != nil, @"DonationURL is not specified.");
+	
+	[[NSWorkspace sharedWorkspace] openURL:[NSURL URLWithString:urlString]];
+}
+
+- (IBAction)cancelDonation:(id)sender
+{
+	[[self window] close];
+}
+
+- (IBAction)donated:(id)sender
+{
+	NSDictionary *bundleInfo = [[NSBundle mainBundle] infoDictionary];
+	NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+	NSArray *verList = [userDefaults objectForKey:@"DonatedVersions"];
+	if (verList == nil) {
+		verList = [NSArray arrayWithObject:[bundleInfo objectForKey:@"CFBundleVersion"]];
+	}
+	else {
+		verList = [verList arrayByAddingObject:[bundleInfo objectForKey:@"CFBundleVersion"]];
+	}
+	[userDefaults setObject:verList forKey:@"DonatedVersions"];
+}
+
+- (IBAction)makeDonation:(id)sender
+{
+	[[self class] goToDonation];
+	[[self window] close];
+}
+
++ (id)displayReminder
+{
+	id newObj = [[self alloc] initWithWindowNibName:@"DonationReminder"];
+	[newObj showWindow:newObj];
+	return newObj;
+}
+
++ (id)remindDonation
+{
+	//return [self displayReminder];
+	NSDictionary *bundleInfo = [[NSBundle mainBundle] infoDictionary];
+	NSUserDefaults *userDefaults = [NSUserDefaults standardUserDefaults];
+	NSArray *verList = [userDefaults objectForKey:@"DonatedVersions"];
+	if (verList == nil) {
+		return [self displayReminder];
+	}
+	else if (![verList containsObject:[bundleInfo objectForKey:@"CFBundleVersion"]]) {
+		return [self displayReminder];
+	}
+	
+	return nil;
+}
+
+- (NSString *)getProductName
+{
+	NSString *localizedStringKey = @"this software";
+	NSString *productName = NSLocalizedString(localizedStringKey,@"");
+	if ([productName isEqualToString:localizedStringKey]) {
+		productName = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleName"];
+	}
+	return productName;
+}
+
+- (void)awakeFromNib
+{
+	NSString *theMessage = [_productMessage stringValue];
+	NSString *productName = [self getProductName];
+	[_productMessage setStringValue: [NSString stringWithFormat:theMessage,productName]];
+	[[self window] center];
+}
+
+@end
